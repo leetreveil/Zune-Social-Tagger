b0ef36870701e13277c85fb77b35248ae9d9a8cb
Lee Treveil <leetreveil@gmail.com>
2009-10-21 22:28:51 +0100

    Sketched out the gui using Sketch-flow

---
+++ docs/SketchFlowProject/SketchFlowProject/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+//
+// SketchFlow needs to know which control assembly contains its screens. This is set automatically
+// on project creation, but if you change the name of the control assembly manually, you must also
+// update it manually here.
+//
+[assembly: Microsoft.Expression.Prototyping.Services.SketchFlowLibraries("SketchFlowProject.Screens")]
+
+namespace SketchFlowProject
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public App()
+		{
+			this.Startup += this.App_Startup;
+		}
+
+		private void App_Startup(object sender, StartupEventArgs e)
+		{
+			this.StartupUri = new Uri(@"pack://application:,,,/Microsoft.Expression.Prototyping.Runtime;Component/WPF/Workspace/PlayerWindow.xaml");
+		}
+	}
+}
---
+++ docs/SketchFlowProject/SketchFlowProject/SketchFlowProjectScreens/Screen 1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SketchFlowProjectScreens
+{
+	/// <summary>
+	/// Interaction logic for Screen_1.xaml
+	/// </summary>
+	public partial class Screen_1 : UserControl
+	{
+		public Screen_1()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
