9989e0b1dcd4ea6c826af4612254b618cef57391
Lee Treveil <leetreveil@gmail.com>
2009-11-04 13:30:21 +0000

    Can now hit return or enter on the search bar and it will invoke the search functionality.

--- src/app/ZuneSocialTagger.GUIV2/SearchBar.xaml.cs
+++ src/app/ZuneSocialTagger.GUIV2/SearchBar.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using ZuneSocialTagger.GUIV2.ViewModels;
+using System.Windows.Input;
 
 namespace ZuneSocialTagger.GUIV2
 {
@@ -12,5 +14,16 @@ namespace ZuneSocialTagger.GUIV2
 		{
 			this.InitializeComponent();
 		}
+
+        private void tbSearch_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            //TODO: do this through databinding
+            if (e.Key == Key.Enter)
+            {
+                var context = (SearchBarViewModel) this.DataContext;
+
+                context.Search();
+            }
+        }
 	}
 }
