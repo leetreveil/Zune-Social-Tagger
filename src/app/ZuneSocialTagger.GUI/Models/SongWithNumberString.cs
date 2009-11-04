db86fe8cee669e42240dd18d96177040356a5d0a
Lee Treveil <leetreveil@gmail.com>
2009-11-04 16:45:56 +0000

    Removed old GUI project. Removed sampledata.

--- src/app/ZuneSocialTagger.GUI/Models/SongWithNumberString.cs
+++
@@ -1,8 +0,0 @@
-namespace ZuneSocialTagger.GUI.Models
-{
-    public class SongWithNumberString
-    {
-        public string Title { get; set; }
-        public string Number { get; set; }
-    }
-}
---
+++ src/app/ZuneSocialTagger.GUIV2/Views/SearchBar.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Controls;
+using ZuneSocialTagger.GUIV2.ViewModels;
+using System.Windows.Input;
+
+namespace ZuneSocialTagger.GUIV2.Views
+{
+    /// <summary>
+    /// Interaction logic for SearchBar.xaml
+    /// </summary>
+    public partial class SearchBar : UserControl
+    {
+        public SearchBar()
+        {
+            this.InitializeComponent();
+        }
+
+        private void tbSearch_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            //TODO: do this through databinding
+            if (e.Key == Key.Enter)
+            {
+                var context = (SearchBarViewModel) this.DataContext;
+
+                context.Search();
+            }
+        }
+    }
+}
