9bc8ae384585c9709692f59ba6539d2cc44a0ca6
Lee Treveil <leetreveil@gmail.com>
2009-11-04 17:10:00 +0000

    Move removal of un-used code.

--- src/app/ZuneSocialTagger.GUIV2/Models/AlbumArtistAndTitleWithUrl.cs
+++
@@ -1,9 +0,0 @@
-namespace ZuneSocialTagger.GUIV2.Models
-{
-    public class AlbumArtistAndTitleWithUrl
-    {
-        public string Artist { get; set; }
-        public string Title { get; set; }
-        public string Url { get; set; }
-    }
-}
