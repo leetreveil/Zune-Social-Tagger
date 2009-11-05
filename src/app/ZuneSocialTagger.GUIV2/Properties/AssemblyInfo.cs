32cc513bfb6300f9461e1f0ae5172fec7c133999
Lee Treveil <leetreveil@gmail.com>
2009-11-05 15:58:50 +0000

    strong key added.

--- src/app/ZuneSocialTagger.GUIV2/Properties/AssemblyInfo.cs
+++ src/app/ZuneSocialTagger.GUIV2/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZuneSocialTagger.GUIV2")]
+[assembly: AssemblyTitle("Zune Social Tagger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZuneSocialTagger.GUIV2")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCompany("Lee Treveil, leetreveil@gmail.com")]
+[assembly: AssemblyProduct("Zune Social Tagger")]
+[assembly: AssemblyCopyright("Copyright ©  2009 Lee Treveil")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
