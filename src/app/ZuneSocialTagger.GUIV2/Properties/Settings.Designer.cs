07ba296df1fadc9b90dfe900a2cce75f50788345
Lee Treveil <leetreveil@gmail.com>
2010-01-22 17:24:39 +0000

    Forgot to set the auto update to be enabled by default.

--- src/app/ZuneSocialTagger.GUIV2/Properties/Settings.Designer.cs
+++ src/app/ZuneSocialTagger.GUIV2/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace ZuneSocialTagger.GUIV2.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool CheckForUpdates {
             get {
                 return ((bool)(this["CheckForUpdates"]));
