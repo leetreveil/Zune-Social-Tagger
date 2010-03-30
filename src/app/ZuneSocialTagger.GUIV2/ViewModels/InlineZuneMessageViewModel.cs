88ca6eb5cd3eb5343073b2df68559da354470ef8
Lee Treveil <leetreveil@gmail.com>
2010-03-31 00:25:36 +0100

    Error / warning messages are now displayed forever until the user clicks ok.

--- src/app/ZuneSocialTagger.GUIV2/ViewModels/InlineZuneMessageViewModel.cs
+++ src/app/ZuneSocialTagger.GUIV2/ViewModels/InlineZuneMessageViewModel.cs
@@ -1,14 +1,22 @@
-using ZuneSocialTagger.GUIV2.Models;
-using System;
+using System.Timers;
 
 namespace ZuneSocialTagger.GUIV2.ViewModels
 {
     public class InlineZuneMessageViewModel : NotifyPropertyChangedImpl
     {
+        //private readonly Timer _timer;
         private string _messageText;
         private ErrorMode _errorMode;
+        //private bool _shouldDisplayMessage;
 
-        public event Action DoShowMessage = delegate { };
+        //public InlineZuneMessageViewModel()
+        //{
+        //    _timer = new Timer();
+        //    _timer.Elapsed += delegate
+        //                          {
+        //                              this.ShouldDisplayMessage = false;
+        //                          };
+        //}
 
         public string MessageText
         {
@@ -30,12 +38,25 @@ namespace ZuneSocialTagger.GUIV2.ViewModels
             }
         }
 
+        //public bool ShouldDisplayMessage
+        //{
+        //    get { return _shouldDisplayMessage; }
+        //    set
+        //    {
+        //        _shouldDisplayMessage = value;
+        //        NotifyOfPropertyChange(() => this.ShouldDisplayMessage);
+        //    }
+        //}
+
         public void ShowMessage(ErrorMode errorMode, string message)
         {
             this.ErrorMode = errorMode;
             this.MessageText = message;
+            //this.ShouldDisplayMessage = true;
 
-            DoShowMessage.Invoke();
+            ////reset the interval each time a message is displayed
+            //_timer.Interval = 5000;
+            //_timer.Start();
         }
     }
 }
