0d51d6302ff873ad53a6be421c89687f37b55efa
Lee Treveil <leetreveil@gmail.com>
2010-05-20 16:49:01 +0100

    Added text to SearchControl.xaml background.

---
+++ src/app/ZuneSocialTagger.GUI/Converters/TextToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace ZuneSocialTagger.GUI.Converters
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class TextToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string)value != "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
